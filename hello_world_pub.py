edcc3e237f28eada07136ad908f49e1533de773c
Jessyca Allen <jessycalynn9@aggiemail.usu.edu>
2021-12-08 12:40:45 -0700

    Push worked, deleting my test comment!

--- hello_world_pub.py
+++ hello_world_pub.py
@@ -3,7 +3,6 @@
 import rospy
 from std_msgs.msg import String
 
-#Just a test to see if I can push  
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
